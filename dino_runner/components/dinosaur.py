f5ebb6756dfc1d3a22c227683b13de754fddfd0e
LaRaraDeLaEsquina <Dayla.Uribe@fundacion-jala.org>
2022-10-05 17:24:21 -0400

    class 1 - Tarea
    esta hermosa actualizacion por Dayla Uribe permite que el dinosaurio ahora realice una accion adicional. Agacharse o en ingles, DUCKING. Es revolucionario!
    
    postdata. los comentarios por aqui y alla son lo que queda de un intento anterior de arreglar un bug. Veanlo si quieren, o no. depende de ustedes :)

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,8 +1,7 @@
 import pygame
 
  
-from dino_runner.utils.constants import RUNNING
-from dino_runner.utils.constants import JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 from pygame.sprite import Sprite
 
 class Dinosaur(Sprite):
@@ -18,7 +17,10 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jum = False
+        #self.dino_to_jump_pos = self.Y_POS
         self.jump_vel = self.JUMP_VEL
+        self.dino_duck = False
+        self.dino_ducking_pos = self.Y_POS + 40
         
 
 
@@ -27,23 +29,35 @@ class Dinosaur(Sprite):
             self.run()
         elif self.dino_jum:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
+        
 
         if user_imput[pygame.K_UP] and not self.dino_jum:
             self.dino_jum = True
             self.dino_run = False
+            self.dino_duck = False
         elif not self.dino_jum:
             self.dino_jum = False
             self.dino_run = True
+            self.dino_duck = False
+
+        if user_imput[pygame.K_DOWN] and not self.dino_jum:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jum = False
 
         if self.step_index >= 10:
             self.step_index = 0
 
 
     def jump(self):
+        print("jump")
         self.image = JUMPING
+        #self. dino_rect.y = self.dino_to_jump_pos
         if self.dino_jum:
             self.dino_rect.y -= self.jump_vel * 4
-            print(self.dino_rect.y)
+            #print(self.dino_rect.y)
             self.jump_vel -= 0.8
             #print(self.jump_vel)
 
@@ -62,7 +76,14 @@ class Dinosaur(Sprite):
 
 
     def duck(self):
-        pass
+        print("duck")
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.dino_ducking_pos
+        self.step_index += 1
+### si rapidamente cambio de jump a duck el dinosaurio baja mucho. ¿como evito eso?
+### resuelto! fue gracias al not en line'47
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
